3913b990529a44ea73622190a30a4b7b6b102c9a
Vishal Yadav <vishalimp2002@gmail.com>
2024-01-09 10:20:21 +0530

    Encapsulation Commit

---
+++ src/p1/Human.java
@@ -0,0 +1,29 @@
+package p1;
+public class Human{
+	public static void main(String[] args) {
+		person p1 = new person();
+		
+		p1.setName("Vishal");
+		p1.setLast("Yadav");
+		p1.setAge(54665);
+		p1.setSalary(58956);
+		
+		System.out.println("Name: " + p1.getName());
+
+        System.out.println("Last: " + p1.getLast());
+        
+        System.out.println("Age: " + p1.getAge());
+
+        System.out.println("Salary: " + p1.salary());
+		
+		
+		
+		
+		
+		
+		
+		
+		
+		
+	}
+}
---
+++ src/p1/person.java
@@ -0,0 +1,41 @@
+package p1;
+public class person {
+	
+	private String name ;
+	private String Last;
+	private int age;
+	private int salary;
+	
+	public void setName(String name) {
+		this.name= name;
+	}
+	 public String getName() {
+		 return name;
+	 }
+	 
+	 public void setLast(String Last) {
+		 this.Last =Last;
+	 }
+	  public String getLast() {
+		  return Last;
+	  }
+	  
+	  public void setAge(int age) {
+		  this.age = age;
+	  }
+	   public int getAge() {
+		   return age;
+	   }
+	
+
+	   public void setSalary(int salary) {
+		   this.salary = salary;
+	   }
+	   public int salary() {
+		   return salary;
+	   }
+
+	}
+
+
+	
